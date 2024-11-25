6ae41443ab5dfa0b38c429a96a1d5b63348a319a
Juan Pelaez <david.pelaezcumbe@gmail.com.co>
2024-11-25 00:50:44 -0500

    feat: lading page created feat: defined routes for view page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import {createFinancialEntitiesRouter} from "./routes/financial-entities-routes.
 import {createCategoriesRouter} from "./routes/categories-routes.js";
 import {createAccountsRouter} from "./routes/accounts-routes.js";
 import {createRevenuesRouter} from "./routes/revenues-routes.js";
+import {ROOT_PAGE} from "./core/domain/root-page.js";
 
 export const createApp = (
     {
@@ -28,6 +29,8 @@ export const createApp = (
     app.use('/api/categories', createCategoriesRouter({ categoriesModel: categoriesModel }))
     app.use('/api/accounts', createAccountsRouter({ accountsModel: accountsModel }))
     app.use('/api/revenues', createRevenuesRouter({ accountsModel: revenuesModel }))
+    app.get('/', (req, res) => res.send(ROOT_PAGE))
+    app.get('**', (req, res) => res.send(ROOT_PAGE))
 
     const PORT = process.env.PORT ?? 1234
 
